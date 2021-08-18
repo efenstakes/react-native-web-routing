ede83a7f422e7a082aec4be01f721e8fbe40859f
Efen Stakes <efenstakes101@gmail.com>
2021-08-18 19:15:01 +0300

    react native web routing

--- App.js
+++ App.js
@@ -1,21 +1,175 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+
+
+function OnboardingPage({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Onboarding Page</Text>
+      <Button title="Home" onPress={()=> navigation.navigate('Home')} ></Button>   
+    </View>
+  );
+}
+function HomePage({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Page</Text>
+      <Button title="Go" onPress={()=> navigation.navigate('Details', { id: 233 })} ></Button>
+      <Button title="MyProfile" onPress={()=> navigation.navigate('Profile', { screen: 'MyProfile' })} ></Button>    
+    </View>
+  );
+}
+function BusinessDetailsPage({ route }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> business id {route.params.id} details Page</Text>
+    </View>
+  );
+}
+function ProfilePage({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> profike Page</Text>
+      <Button title="Update Profile" onPress={()=> navigation.navigate('Profile', { screen: 'MyProfileUpdate' })} ></Button> 
+      <Button title="Login" onPress={()=> navigation.navigate('Profile', { screen: 'Login' })} ></Button>
+      <Button title="Update Password" onPress={()=> navigation.navigate('Profile', { screen: 'MyProfileUpdatePassword' })} ></Button>   
+    </View>
+  );
+}
+function UpdateProfilePage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>update profike Page</Text>
+    </View>
+  );
+}
+function UpdateProfilePasswordPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>update profike password Page</Text>
+    </View>
+  );
+}
+function LoginPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Page</Text>
+    </View>
+  );
+}
+function  CreateAccountPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Create Account Page</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+function  CreateBusinessPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Create Business Page</Text>
+    </View>
+  );
+}
+function  HowToBecomeABusinessPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> How to become a business Account Page</Text>
+    </View>
+  );
+}
+function  BusinessUpdatePage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Create Account Page</Text>
+    </View>
+  );
+}
+
+function  NotFoundPage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> NotFoundPage Page</Text>
+    </View>
+  );
+}
+
+
+const linking = {
+  // prefixes: [
+  // ],
+  config: {
+    screens: {
+      TutorialScreen: 'tutorial',
+      Home: '',
+      Details: 'business/:id',
+      Profile: {
+        path: 'profile',
+        screens: {
+          MyProfile: 'my',
+          MyProfileUpdate: 'update',
+          MyProfileUpdatePassword: 'password',
+          Login: {
+            path: 'login',
+            exact: true,
+          },
+          CreateAccount: {
+            path: 'create-account',
+            exact: true,
+          },
+        },
+      },
+      NotFound: "*"
+    },
   },
-});
+};
+
+
+const ProfileStack = createNativeStackNavigator();
+const ProfileDrawer = createDrawerNavigator()
+const Stack = createNativeStackNavigator();
+
+
+const ProfileDrawerComponent = ()=> {
+  return (
+    <ProfileDrawer.Navigator>
+      <ProfileDrawer.Screen name="MyProfile" component={ProfilePage} />
+      <ProfileDrawer.Screen name="MyProfileUpdate" component={UpdateProfilePage} />
+      <ProfileDrawer.Screen name="MyProfileUpdatePassword" component={UpdateProfilePasswordPage} />
+      <ProfileDrawer.Screen name="CreateAccount" component={CreateAccountPage} />
+      <ProfileDrawer.Screen name="Login" component={LoginPage} />
+    </ProfileDrawer.Navigator>
+  )
+}
+const ProfileStackComponent = ()=> {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="MyProfile" component={ProfilePage} />
+      <ProfileStack.Screen name="MyProfileUpdate" component={UpdateProfilePage} />
+    </ProfileStack.Navigator>
+  )
+}
+export default function App() {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Details" component={BusinessDetailsPage} />
+
+
+        <Stack.Screen name="Profile" component={ProfileDrawerComponent} />
+        {/* NotFoundPage */}
+        
+        <Stack.Screen name="NotFound" component={NotFoundPage} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
